bca2cca1ff1c226ab3d5521764f037aa7736c0d3
Biihuy <biihuy25697@gmail.com>
2020-07-21 17:30:27 +0700

    2107 530 update

---
+++ js/mainjs.js
@@ -0,0 +1,35 @@
+$('.mv-slick').slick({
+	dots: true,
+	arrows: false,
+	autoplay: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplaySpeed: 2000,
+});
+$('.special-bg').slick({
+	dots: false,
+	arrows: false,
+	autoplay: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplaySpeed: 2000,
+});
+$('.people-underview').slick({
+	dots: false,
+	arrows: false,
+	autoplay: true,
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	autoplaySpeed: 2000,
+});
+$('.people-slick').slick({
+	dots: false,
+	arrows: true,
+	prevArrow:'<button class="slick-arrow slickPrev"></button>',
+	nextArrow:'<button class="slick-arrow slickNext"></button>',
+	autoplay: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplaySpeed: 2000,
+});
+
